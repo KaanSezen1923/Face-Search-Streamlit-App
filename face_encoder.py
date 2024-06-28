b18bcb5572817cf948d7c7237853c16ad0da6db9
Kaan Sezen <119515258+KaanSezen1923@users.noreply.github.com>
2024-06-29 01:51:57 +0300

    Update face_encoder.py

--- face_encoder.py
+++ face_encoder.py
@@ -3,7 +3,7 @@ import face_recognition
 import pickle 
 import cv2 
 
-folder_path="C:/Users/KAAN/Desktop/Data"
+folder_path="veri setinizin yolunu girin."
 pathlist=os.listdir(folder_path)
 print(pathlist)
 images=[]
